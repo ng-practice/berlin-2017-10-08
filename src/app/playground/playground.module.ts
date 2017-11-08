8f795de1d56eed198565fcc0a4a07c995e1e72e1
Gregor Woiwode <gregor.woiwode@gmail.com>
2017-11-08 13:56:18 +0100

    add mouse-cursor

--- src/app/playground/playground.module.ts
+++ src/app/playground/playground.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InfoBoxComponent } from './info-box/info-box.component';
+import { MouseCursorComponent } from './mouse-cursor/mouse-cursor.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [InfoBoxComponent],
-  exports: [InfoBoxComponent]
+  declarations: [InfoBoxComponent, MouseCursorComponent],
+  exports: [InfoBoxComponent, MouseCursorComponent]
 })
 export class PlaygroundModule { }
